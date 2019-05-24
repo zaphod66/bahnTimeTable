0d7a1d205b192b8986f9ed861881393b2d5e40af
Norbert Scheller <norbert.scheller@gmail.com>
2019-05-24 22:04:56 +0200

    master - small clean up

--- play-app/public/javascripts/timetable.js
+++ play-app/public/javascripts/timetable.js
@@ -14,26 +14,28 @@ const timetable = new Vue({
 		port: location.port,
 		counter: 0,
 		counterVisible: false,
-		tableVisible: false
+		tableVisible: false,
+		buttonsVisible: false
 	},
 	methods: {
 		handleInput: function() {
 		},
-		submitForm() {
+		submitForm: function() {
 		    var encoded = encodeURIComponent(this.response)
             var url     = this.protocol + '//' + this.hostname + ':' + this.port + "/betriebJson/" + this.inputValue
             this.counter = 0
             this.tableVisible = false
+            this.buttonsVisible = false
 
             axios
               .get(url)
               .then(response => (this.stations = response.data))
 		},
-		checkData() {
+		checkData: function() {
             var arrayLength = this.stations.length
             this.stations.forEach(this.checkDs100)
 		},
-		checkDs100(station) {
+		checkDs100: function(station) {
 		    var url = this.protocol + '//' + this.hostname + ':' + this.port + "/stationJson/" + station.ds100
             this.counterVisible = true
 		    axios
@@ -42,21 +44,29 @@ const timetable = new Vue({
 		          var eva = response.data.eva
 		          var url = this.protocol + '//' + this.hostname + ':' + this.port + "/timeTable/" + eva
 		          var lnk = '<a href="' + url + '" target="_blank">' + station.name + '</a>'
-//		          var btn = '<button class="btn submit" v-on:click="getTimeTable(' + eva + ')">' + station.name + '</button>'
-//                  var btn = '<button class="btn submit" v-on:click="getTimeTable($st.ds100)">{{st.ds100}}</button>'
                   var btn = '<button class="btn submit" v-on:click="getTimeTable($st.ds100)">{{st.ds100}}</button>'
-//		          station.name  = lnk
 		          station.link  = lnk
-		          station.ds100 = eva
 		          station.eva   = eva
+                  station.found = true
                   this.counter = this.counter + 1
-                  if (this.counter == this.stations.length) { this.counterVisible = false; this.counter = 0 }
+                  if (this.counter == this.stations.length) {
+                    this.counterVisible = false
+                    this.counter = 0
+                    this.buttonsVisible = true
+                    this.stations = this.stations.filter(s => s.found)
+                  }
 		        })
 		        .catch(_ => {
     		      station.name  = '<strike>' + station.name + '</strike>'
     		      station.ds100 = '<strike>' + station.ds100 + '</strike>'
+                  station.found = false
                   this.counter = this.counter + 1
-                  if (this.counter == this.stations.length) { this.counterVisible = false; this.counter = 0 }
+                  if (this.counter == this.stations.length) {
+                    this.counterVisible = false
+                    this.counter = 0
+                    this.buttonsVisible = true
+                    this.stations = this.stations.filter(s => s.found)
+                  }
 		        })
 		},
 		getTimeTable: function(eva) {
